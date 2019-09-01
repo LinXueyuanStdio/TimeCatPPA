2e775371ac261939175f61c3ce7514585c58a37a
兮尘 <761516186@qq.com>
2019-09-02 05:54:18 +0800

    release

--- generate.sh
+++ generate.sh
@@ -18,7 +18,7 @@ export KEYNAME=761516186@qq.com
     mv Packages.gz ../dists/timecat/stable/binary-aarch64/Packages.gz
 
     # Release, Release.gpg & InRelease
-    apt-ftparchive -c ../apt.config release . > Release
+    apt-ftparchive -c ../apt.config release ../dists/timecat > Release
     gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
     gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
 
