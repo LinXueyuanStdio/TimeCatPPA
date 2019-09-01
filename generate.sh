d049d9006cd8d8c5594908900188bd97c88955b1
兮尘 <761516186@qq.com>
2019-09-02 05:36:22 +0800

    update

--- generate.sh
+++ generate.sh
@@ -9,14 +9,20 @@ export KEYNAME=761516186@qq.com
     set -e
     set -v
 
-    cd ./shell/aarch64/
+    cd ./aarch64/
 
     # Packages & Packages.gz
     dpkg-scanpackages --multiversion . > Packages
     gzip -k -f Packages
+    mv Packages ../dists/timecat/stable/binary-aarch64/Packages
+    mv Packages.gz ../dists/timecat/stable/binary-aarch64/Packages.gz
 
     # Release, Release.gpg & InRelease
-    apt-ftparchive -c ../../apt.config release . > Release
+    apt-ftparchive -c ../apt.config release . > Release
     gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
     gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
+
+    mv Release ../dists/timecat/Release
+    mv Release.gpg ../dists/timecat/Release.gpg
+    mv InRelease ../dists/timecat/InRelease
 )
