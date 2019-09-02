761b42d9d4d6e09029dd53f52d8320591084892d
兮尘 <761516186@qq.com>
2019-09-02 12:11:42 +0800

    packs

--- generate.sh
+++ generate.sh
@@ -5,24 +5,17 @@ set -v
 
 export KEYNAME=761516186@qq.com
 
-(
-    set -e
-    set -v
+# Packages & Packages.gz
+dpkg-scanpackages --multiversion aarch64 > Packages
+gzip -k -f Packages
+mv Packages dists/timecat/stable/binary-aarch64/Packages
+mv Packages.gz dists/timecat/stable/binary-aarch64/Packages.gz
 
-    cd ./aarch64/
+# Release, Release.gpg & InRelease
+apt-ftparchive -c apt.config release dists/timecat > Release
+gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
+gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
 
-    # Packages & Packages.gz
-    dpkg-scanpackages --multiversion . > Packages
-    gzip -k -f Packages
-    mv Packages ../dists/timecat/stable/binary-aarch64/Packages
-    mv Packages.gz ../dists/timecat/stable/binary-aarch64/Packages.gz
-
-    # Release, Release.gpg & InRelease
-    apt-ftparchive -c ../apt.config release ../dists/timecat > Release
-    gpg --default-key "${KEYNAME}" -abs -o - Release > Release.gpg
-    gpg --default-key "${KEYNAME}" --clearsign -o - Release > InRelease
-
-    mv Release ../dists/timecat/Release
-    mv Release.gpg ../dists/timecat/Release.gpg
-    mv InRelease ../dists/timecat/InRelease
-)
+mv Release dists/timecat/Release
+mv Release.gpg dists/timecat/Release.gpg
+mv InRelease dists/timecat/InRelease
